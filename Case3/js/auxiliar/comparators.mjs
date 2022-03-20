c0387fc45244bf57054c3e514df39b0f9691d2b9
Samuel <samuel.pcastro@outlook.com>
2022-03-20 21:50:39 +0100

    case3 change

--- Case3/js/auxiliar/comparators.mjs
+++ Case3/js/auxiliar/comparators.mjs
@@ -1,4 +1,6 @@
 // La siguiente función es empleada por el método sort de un array.
 // Informate sobre los requisitos que ha de cumplir la función.
-export function compareRunners () {
+export function compareRunners (a, b) {
+    // el Array runners como objeto llama a su propiedad time
+   return a.time - b.time
 }
